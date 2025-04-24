4b79053ad41c24c03c8deb367c3cf103bc4d59a4
JAPV-X2612 <jesus.pinzon-v@mail.escuelaing.edu.co>
2025-04-24 03:27:58 -0500

    New Files by Jesus Pinzon

---
+++ src/main/java/edu/eci/cvds/auth/AuthServiceApplication.java
@@ -0,0 +1,12 @@
+package edu.eci.cvds.auth;
+
+/**
+ * Hello world!
+ *
+ */
+public class AuthServiceApplication {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
