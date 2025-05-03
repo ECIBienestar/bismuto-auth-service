641bc9385636c8ad506763ce4e5f4da45f28ea85
CamiloFdez <camilo.fernandez-d@mail.escuelaing.edu.co>
2025-05-03 18:37:47 -0500

    feat: Fixing errors

--- src/main/java/edu/eci/cvds/auth/dto/UserResponseDTO.java
+++ src/main/java/edu/eci/cvds/auth/dto/UserResponseDTO.java
@@ -11,36 +11,4 @@ public class UserResponseDTO {
     private String id;
     private String name;
     private String email;
-
-    public UserResponseDTO() {}
-
-    public UserResponseDTO(String name, String email, String id) {
-        this.name = name;
-        this.email = email;
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 }
