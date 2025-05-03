62f490a369f5729f183eaa6e9bed3f210ae59c90
CamiloFdez <camilo.fernandez-d@mail.escuelaing.edu.co>
2025-05-03 16:49:19 -0500

    feat: Fixing errors of sonarcloud and code repeated

--- src/main/java/edu/eci/cvds/auth/dto/UserRegisterDTO.java
+++ src/main/java/edu/eci/cvds/auth/dto/UserRegisterDTO.java
@@ -1,40 +1,14 @@
 package java.edu.eci.cvds.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponseDTO {
     private String id;
     private String name;
     private String email;
-
-    public UserResponseDTO() {
-    }
-
-    public UserResponseDTO(String id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
